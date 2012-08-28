f12a0e8524f4a6bb53deb88ebfedc569aaa9dbab
Roman Yudin <r@wobot.co>
2012-08-28 18:42:49 +0400

    testing update
    first test cases
    
    Signed-off-by: Roman Yudin <r@wobot.co>

--- test.js
+++ test.js
@@ -2,8 +2,14 @@ var analyze = require('./lib/sentimental').analyze,
     positivity = require('./lib/sentimental').positivity,
     negativity = require('./lib/sentimental').negativity;
 
-an=analyze("Hey you worthless scumbag"); //Score: -6, Comparative:-1.5
-pos=positivity("This is so cool"); //Score: 1, Comparative:.25
-neg=negativity("Hey you worthless scumbag"); //Score: 6, Comparative:1.5
+an=analyze("Hey you worthless scumbag");
+pos=positivity("This is so cool");
+neg=negativity("Hey you worthless scumbag");
+an_ru=analyze("Чо за хуйня?");
+an_ru2=analyze("Ненавижу это гавно");
+an_ru3=analyze("Система автоопределения тональности явно недооценена");
 
 console.log(an);
+console.log(an_ru);
+console.log(an_ru2);
+console.log(an_ru3);
