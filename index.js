9b0b4f83b264a8be623f4e452dc752ea207100a1
KR <kr@uber.com>
2014-04-12 10:23:52 -0700

    cleaning up extra logger

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var analyze = require('./lib/sentimental').analyze,
     negativity = require('./lib/sentimental').negativity,
     positivity = require('./lib/sentimental').positivity;
 
-kr = "cool beans";
-console.log(analyze(kr))
-
 module.exports = {
   analyze    : analyze,
   negativity : negativity,
