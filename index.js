feea2e06cb7a0944870ed0fffd5abca066a79166
KR <kr@uber.com>
2014-04-12 08:44:08 -0700

    Improving underlying data structures

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var analyze = require('./lib/sentimental').analyze,
     negativity = require('./lib/sentimental').negativity,
     positivity = require('./lib/sentimental').positivity;
 
+kr = "cool beans";
+console.log(analyze(kr))
+
 module.exports = {
   analyze    : analyze,
   negativity : negativity,
